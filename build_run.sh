8da6a8ee1366b58203ef3071d47d44f1fe971552
jingjingcai <jcai28@usfca.edu>
2024-04-09 23:35:55 -0700

    working image

--- build_run.sh
+++ build_run.sh
@@ -4,14 +4,9 @@
 docker-compose down
 
 # Change directory to spark-master
-cd spark-master || exit
+cd spark || exit
 # Build spark-master image
-docker build -t spark-master .
-
-# Change directory to spark-worker
-cd ../spark-worker || exit
-# Build spark-worker image
-docker build -t spark-worker .
+docker build -t spark-image .
 
 # Change back to the main directory
 cd ..
--- spark/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set the Spark Master host
-export SPARK_MASTER_HOST=$(hostname)
-
-# Start the Spark Master
-/opt/spark/bin/spark-class org.apache.spark.deploy.master.Master
